7deead747ac11ebfda675216f73889a52eb7e14f
drootang <drootang@users.noreply.github.com>
2022-10-15 10:55:53 -0400

    Update pedit.sh
    Added support for optionally specifying a different path env to modify

--- pedit.sh
+++ pedit.sh
@@ -1,14 +1,21 @@
 # Split each path on PATH onto its own line and allow user to edit.
 # Then join and assign to PATH.
 # This script must be *sourced* not executed, e.g., `alias pedit="~/sw/bin/pedit.sh"`
+# Optionally specify a different env, e.g.,
+#   pedit PYTHONPATH
+if [[ -n "$1" ]] ; then
+    VAR=$1
+else
+    VAR=PATH
+fi
 TMP=$(mktemp $USER-pedit.XXXXXXXXXX)
-echo $PATH | sed 's/:/\n/g' > $TMP
+echo ${!VAR} | sed 's/:/\n/g' > $TMP
 $EDITOR $TMP
 DATA=$(paste -sd : $TMP)
-if [[ -z ${DATA// /} ]] || [[ "$PATH" == "$DATA" ]] ; then
-    echo "PATH not changed"
+if [[ -z ${DATA// /} ]] || [[ "${!VAR}" == "$DATA" ]] ; then
+    echo "${!VAR} not changed"
 else
-    export PATH=$DATA
-    echo "PATH updated"
+    declare "$VAR=$DATA"
+    echo "${!VAR} updated"
 fi
 rm -f "$TMP"
